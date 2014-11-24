2e51b13885df4705e4dfc83615277e2aadfcb32b
engeld <engel.dana.mail@gmail.com>
2014-11-24 11:17:54 +0100

    fleshing out the basic file-reading structure

--- autoblogger.c
+++ autoblogger.c
@@ -1,11 +1,32 @@
 #include <stdio.h>
+#include <errno.h>
 
-int main (void)
+int main(void)
 {
-        char     linktxt_path[256];   /* path to the location of your link.txt */
+    char        item[], status;
+    FILE        *fp;
 
-        scanf("%s",linktxt_path);
-        printf("%s\n",linktxt_path);
+    if((fp = fopen("P:\\links.txt", "r+")) == NULL) {
+        printf("No such file\n");
+        exit(1);
+    }
 
-        return 0;
+    while(1) {
+        int ret = fscanf(fp, "%s %c", item, &status);
+        if(ret == 2) {
+            printf("\n%s \t %c", item, status);
+        } else if(errno != 0) {
+            perror("scanf: ");
+            break;
+        } else if(ret == EOF) {
+            break;
+        } else {
+            printf("No match!\n");
+        }
+    }
+    printf("\n");
+    if(feof(fp)) {
+        puts("EOF");
+    }
+    return 0;
 }
