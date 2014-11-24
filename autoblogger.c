9bcb712b0dd3e8e5865a52de6b6331fbde1eb9b9
engeld <engel.dana.mail@gmail.com>
2014-11-24 10:49:50 +0100

    Create autoblogger.c
    Add coding skelleton, flesh the details out at a later point.

---
+++ autoblogger.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main (void)
+{
+        char     linktxt_path[256];   /* path to the location of your link.txt */
+
+        scanf("%s",linktxt_path);
+        printf("%s\n",linktxt_path);
+
+        return 0;
+}
