4ca9f6869a7eaf5bef69864abc7bd29225c3b038
Dana Engel <engel.dana.mail@gmail.com>
2014-12-26 16:39:05 -0500

    show hint on how to use when called with wrong parameters

--- src/autoblogger.c
+++ src/autoblogger.c
@@ -2,32 +2,15 @@
 #include <stdlib.h>
 #include <errno.h>
 
-int main(void)
+int main( int argc, char *argv[] )
 {
-    char        item[] = "", status;
-    FILE        *fp;
+  char        item[] 	= "", status;
+  FILE        *fp;
 
-    if((fp = fopen("./links.txt", "r+")) == NULL) {
-        printf("No such file\n");
-        exit(1);
-    }
-
-    while(1) {
-        int ret = fscanf(fp, "%s", item);
-        if(ret == 1) {
-            printf("\n%s", item);
-        } else if(errno != 0) {
-            perror("scanf: ");
-            break;
-        } else if(ret == EOF) {
-            break;
-        } else {
-            printf("No match!, retcode: %i\n", ret);
-        }
-    }
-    printf("\n");
-    if(feof(fp)) {
-        puts("EOF");
-    }
-    return 0;
+  if( argc != 3 ){
+    fprintf( stderr, "Usage: %s <link.txt>\n", argv[0]);
+    exit( EXIT_FAILURE );
+  }
+  
+  return EXIT_SUCCESS;
 }
