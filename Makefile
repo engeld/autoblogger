afec5411e2aa177900ce66ecf9300ba90d311cbb
Dana Engel <engel.dana.mail@gmail.com>
2014-12-28 18:38:48 +0100

    add comments and include the headerfile of html.c

--- Makefile
+++ Makefile
@@ -1,7 +1,15 @@
 CC=/usr/local/bin/gcc-4.9
+CARGS=-O3 -g -c -ansi -Wall -pedantic
+LARGS=
 
-blog: src/autoblogger.c
-	$(CC) -g -o blog src/autoblogger.c
+blog: autoblogger.o html.o
+	$(CC) -o blog autoblogger.o html.o $(LARGS)
+
+autoblogger.o: src/autoblogger.c src src/html.h
+	$(CC) -o autoblogger.o src/autoblogger.c $(CARGS)
+
+html.o: src/html.c src/html.h
+	$(CC) -o html.o src/html.c $(CARGS)
 
 clean:
-	rm -f blog
+	rm -f blog && rm -f *.o
--- src/autoblogger.c
+++ src/autoblogger.c
@@ -2,22 +2,28 @@
 #include <stdlib.h>
 #include <errno.h>
 
+#include "html.h"
+
 int main( int argc, char *argv[] )
 {
   FILE        *in;
   char        buf[BUFSIZ];
 
+  /* Handle arguments */
   if( argc != 2 ){
     fprintf( stderr, "Usage: %s <link.txt>\n", argv[0]);
     exit( EXIT_FAILURE );
   }
 
+  /* Handle file-opening */
   in = fopen( argv[1], "r");
   if( in == NULL ){
     perror( "Unable to open the file" );
     exit( EXIT_FAILURE );
   }
   
+  /* depending on the string, handle the printing different */
+  /* may use a switch-case? */
   fread( buf, 1, sizeof( buf ), in);
   printf( "%s\n", buf );
 
